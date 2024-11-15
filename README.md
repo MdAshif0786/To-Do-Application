944723c8aceac85417583832bcfd324964edc736
Talib8335 <mdtalib8335@gmail.com>
2024-11-16 00:15:30 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # To-Do-Application
 A simple and intuitive To-Do application built to help users organize tasks efficiently. 
+
---
+++ script.js
@@ -0,0 +1,34 @@
+const Inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("List-container");
+
+
+function AddTask(){
+    if(Inputbox.value==''){
+        alert('you must write something');
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML=Inputbox.value;
+        listcontainer.appendChild(li);
+        let span =document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    Inputbox.value=" ";
+    savedata();
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showtask();
